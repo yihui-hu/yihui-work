bb081a8af80d74e67f2913e3e8c548f348c09711
yihui <90987235+yihui-hu@users.noreply.github.com>
2023-06-10 12:18:00 -0400

    Testing GitHub actions

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,5 @@ import react from "@astrojs/react";
 // https://astro.build/config
 export default defineConfig({
   site: 'https://yihui-hu.github.io',
-  base: '/yihui-work',
   integrations: [react()],
 });
