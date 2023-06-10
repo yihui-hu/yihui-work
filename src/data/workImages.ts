dc58dee1a38553f240cedb0aebc9804fb570675d
yihui <90987235+yihui-hu@users.noreply.github.com>
2023-06-10 18:06:29 -0400

    Updated static data

--- src/data/workImages.ts
+++ src/data/workImages.ts
@@ -6,77 +6,77 @@ interface WorkImageData {
 
 const workImages: WorkImageData[] = [
   {
-    url: "/src/assets/worksOverview/20XX_poster.webp",
+    url: "/src/assets/works/20XX_poster.webp",
     id: "20XX",
     category: "art design",
   },
   {
-    url: "/src/assets/worksOverview/arith_desktop.webp",
+    url: "/src/assets/works/arith_desktop.webp",
     id: "arith",
     category: "code",
   },
   {
-    url: "/src/assets/worksOverview/everyday_trash.webp",
+    url: "/src/assets/works/everyday_trash.webp",
     id: "everyday__",
     category: "art",
   },
   {
-    url: "/src/assets/worksOverview/famicase.webp",
+    url: "/src/assets/works/famicase.webp",
     id: "famicase",
     category: "art design",
   },
   {
-    url: "/src/assets/worksOverview/figma_plugin.webp",
+    url: "/src/assets/works/figma_plugin.webp",
     id: "f-c-d",
     category: "code design",
   },
   {
-    url: "/src/assets/worksOverview/goodreads_overview.webp",
+    url: "/src/assets/works/goodreads_overview.webp",
     id: "goodreads",
     category: "design",
   },
   {
-    url: "/src/assets/worksOverview/graphite_allegory.webp",
+    url: "/src/assets/works/graphite_allegory.webp",
     id: "g-r",
     category: "art",
   },
   {
-    url: "/src/assets/worksOverview/henesys_tags.webp",
+    url: "/src/assets/works/henesys_tags.webp",
     id: "henesys",
     category: "code design",
   },
   {
-    url: "/src/assets/worksOverview/misc_water.webp",
+    url: "/src/assets/works/misc_water.webp",
     id: "misc.",
     category: "design art",
   },
   {
-    url: "/src/assets/worksOverview/pidgin.webp",
+    url: "/src/assets/works/pidgin.webp",
     id: "pidgin",
     category: "code",
   },
   {
-    url: "/src/assets/worksOverview/pokesearch_duo.webp",
+    url: "/src/assets/works/pokesearch_duo.webp",
     id: "pokéSearch",
     category: "code",
   },
   {
-    url: "/src/assets/worksOverview/s-i-l_grid.webp",
+    url: "/src/assets/works/s-i-l_grid.webp",
     id: "s-i-l",
     category: "code",
   },
   {
-    url: "/src/assets/worksOverview/y0p_veiled.webp",
+    url: "/src/assets/works/y0p_veiled.webp",
     id: "y0p",
     category: "code",
   },
   {
-    url: "/src/assets/worksOverview/yhhu_xyz_desktop.webp",
+    url: "/src/assets/works/yhhu_xyz_desktop.webp",
     id: "yhhu.xyz",
     category: "code design art",
   },
   {
-    url: "/src/assets/worksOverview/ZINEDEF_stories.webp",
+    url: "/src/assets/works/ZINEDEF_stories.webp",
     id: "ZINEDEF",
     category: "art design",
   },
