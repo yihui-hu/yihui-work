c589dc5d9f05c6b14cb1959ddd5f323a428b3c74
yihui <90987235+yihui-hu@users.noreply.github.com>
2023-06-10 21:53:34 -0400

    Updated config files

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,8 @@
 import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
+import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://yihui.work',
-  integrations: [react()],
+  integrations: [react(), mdx()]
 });
