4ce6d2638835ba500ad9656f6c692b651ebb1d39
yihui <90987235+yihui-hu@users.noreply.github.com>
2023-06-11 00:09:28 -0400

    Removed vercel adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,8 @@
 import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
 import mdx from "@astrojs/mdx";
-import vercel from '@astrojs/vercel/serverless';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), mdx()],  
-  output: 'server',
-  adapter: vercel(),
+  integrations: [react(), mdx()],
 });
